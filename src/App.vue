441ee39583ab64e0ea30dbb655592bc389807224
Aleksey <a.sviridov@idaproject.com>
2021-12-12 23:53:18 +0400

    feat fetch

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="wrapper"> 
+    <h1 class="h1">{{ title }}</h1>
+
+    <Card class="card"
+          :list="moviesList.data"
+    />
+
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Card,
+  },
+
+  data() {
+    return {
+      title: 'Movies by Aleksey',
+      moviesList: []
+    }
+  },
+
+  async mounted() {
+      let response = await fetch('https://kinobd.ru/api/films', {
+        method: 'GET', 
+      });
+      this.moviesList = await response.json();   
+  },
+
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    max-width: 1920px;
+    margin: 0 auto
+  }
+  .h1 {
+    text-align: center;
+  }
+
+  .button {
+    display: block;
+    width: 10%;
+    height: 35px;
+    margin: 0 auto;
+    color: #fff;
+    background-color: brown;
+    border-radius: 15px;
+    border: none;
+    cursor: pointer;
+
+    &:hover {
+      background-color: red;
+    }
+  }
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="card-row">
+        <div class="card"
+             v-for="item in list"
+             :key="item.id"
+        >            
+            <img class="poster" :src="item.big_poster">   
+            <h2 class="title">{{ item.name_russian }}</h2>        
+        </div>        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+
+    props: {
+        list: {
+            type: Array,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .card-row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .card {
+        position: relative;
+        width: 300px;
+        height: 350px;
+        border: 1px solid green;
+    }
+
+    .title {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 1;
+    }
+
+    .poster {
+        width: 100%;
+        height: 100%;
+    }
+</style>
